8787a04ac7cbca23427ec8c9376bd4cebde45024
codehroll <judyh0665@gmail.com>
2024-12-06 12:35:34 -0800

    delete console log in moduleDao

--- Kanbas/Modules/dao.js
+++ Kanbas/Modules/dao.js
@@ -8,9 +8,9 @@ export function findModulesForCourse(courseId) {
 }
 
 export function createModule(module) {
-  // delete module._id;
+  delete module._id;
   const newModule = model.create(module);
-  console.log("create current module: ", newModule._id);
+  // console.log("create current module: ", newModule._id);
   return newModule;
   // const newModule = { ...module, _id: Date.now().toString() };
   // Database.modules = [...Database.modules, newModule];
@@ -18,18 +18,16 @@ export function createModule(module) {
 }
 
 export function deleteModule(moduleId) {
-  // const curModuleObjectId = new mongoose.Types.ObjectId(moduleId);
-  console.log("delete current module: ", moduleId);
+  // console.log("delete current module: ", moduleId);
   return model.deleteOne({ _id: moduleId });
   // const { modules } = Database;
   // Database.modules = modules.filter((module) => module._id !== moduleId);
 }
 
 export function updateModule(moduleId, moduleUpdates) {
-  console.log("update current module: ", moduleId);
-  // const curModuleObjectId = new mongoose.Types.ObjectId();
-  // curModuleObjectId = mongoose.Types.ObjectId(moduleId);
+  // console.log("update current module: ", moduleId);
   return model.updateOne({ _id: moduleId }, moduleUpdates);
+
   // const { modules } = Database;
   // const module = modules.find((module) => module._id === moduleId);
   // Object.assign(module, moduleUpdates);
